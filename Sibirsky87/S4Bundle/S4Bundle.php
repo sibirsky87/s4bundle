0a43b9eadf4dfb097f8b0e8a3b1fb18214e90981
Anton Baikov <baikov87@gmail.com>
2018-12-06 19:18:20 +0700

    init

---
+++ Sibirsky87/S4Bundle/S4Bundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sibirsky87\S4Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class S4Bundle extends Bundle
+{
+    //put your code here
+}
---
+++ Sibirsky87/S4Bundle/Service/S4service.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sibirsky87\S4Bundle\Service;
+
+class S4service
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function getIt()
+    {
+        return 'it';
+    }
+}
